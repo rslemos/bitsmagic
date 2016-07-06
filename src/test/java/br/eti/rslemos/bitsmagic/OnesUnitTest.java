bb58923c812131d66788489f7755bff697f1fde0
R. Lemos <rslemos@gmail.com>
2016-07-06 00:01:37 -0300

    bitcount for arrays test cases

--- src/test/java/br/eti/rslemos/bitsmagic/OnesUnitTest.java
+++ src/test/java/br/eti/rslemos/bitsmagic/OnesUnitTest.java
@@ -27,7 +27,6 @@
  *******************************************************************************/
 package br.eti.rslemos.bitsmagic;
 
-import static br.eti.rslemos.bitsmagic.Ones.ones;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -200,5 +199,118 @@ public class OnesUnitTest {
 		public Long() { super(java.lang.Long.SIZE); }
 		@Override protected java.lang.Long cast(long x) { return (long)x; }
 		@Override protected int ones(java.lang.Long x) { return Ones.ones(x); }
+	}
+
+	private abstract static class Fixture<T> implements StorageBuilder<T> {
+		//                         3210987654321098765432109876543210987654321098765432109876543210
+		public T subject = build(0b0011001100110110100000001000000100000100001000100010010010101010L,
+		                         0b1100110011001001011111110111111011111011110111011101101101010101L,
+		                         0b0101010101011011100000011000001100001100011001100110110111111110L,
+		                         0b0101010101010010001111110011111001111001110011001100100100000000L,
+		                         0b0110001111001001111110000010111001110001000001111110111000000010L,
+		                         0b0011111101111100001001000010111111101110001000001000001110110101L);
+	}
+	
+	@Ignore
+	public abstract static class Cases<T> extends Fixture<T> {
+		protected abstract int ones(T data, int from, int to);
+
+		@Test public void ones_0_0()    { assertThat(ones(subject,  0,        0), is(equalTo(  0))); }
+		@Test public void ones_0_4()    { assertThat(ones(subject,  0,        4), is(equalTo(  2))); }
+		@Test public void ones_0_6()    { assertThat(ones(subject,  0,        6), is(equalTo(  4))); }
+		@Test public void ones_0_7()    { assertThat(ones(subject,  0,        7), is(equalTo(  4))); }
+		@Test public void ones_0_8()    { assertThat(ones(subject,  0,        8), is(equalTo(  5))); }
+		@Test public void ones_0_12()   { assertThat(ones(subject,  0,       12), is(equalTo(  7))); }
+		@Test public void ones_0_14()   { assertThat(ones(subject,  0,       14), is(equalTo(  7))); }
+		@Test public void ones_0_15()   { assertThat(ones(subject,  0,       15), is(equalTo(  7))); }
+		@Test public void ones_0_16()   { assertThat(ones(subject,  0,       16), is(equalTo(  8))); }
+		@Test public void ones_0_20()   { assertThat(ones(subject,  0,       20), is(equalTo(  8))); }
+		@Test public void ones_0_22()   { assertThat(ones(subject,  0,       22), is(equalTo(  9))); }
+		@Test public void ones_0_23()   { assertThat(ones(subject,  0,       23), is(equalTo(  9))); }
+		@Test public void ones_0_24()   { assertThat(ones(subject,  0,       24), is(equalTo(  9))); }
+		@Test public void ones_0_28()   { assertThat(ones(subject,  0,       28), is(equalTo( 12))); }
+		@Test public void ones_0_30()   { assertThat(ones(subject,  0,       30), is(equalTo( 13))); }
+		@Test public void ones_0_31()   { assertThat(ones(subject,  0,       31), is(equalTo( 14))); }
+		@Test public void ones_0_32()   { assertThat(ones(subject,  0,       32), is(equalTo( 15))); }
+		@Test public void ones_0_36()   { assertThat(ones(subject,  0,       36), is(equalTo( 19))); }
+		@Test public void ones_0_40()   { assertThat(ones(subject,  0,       40), is(equalTo( 20))); }
+		@Test public void ones_0_44()   { assertThat(ones(subject,  0,       44), is(equalTo( 21))); }
+		@Test public void ones_0_46()   { assertThat(ones(subject,  0,       46), is(equalTo( 22))); }
+		@Test public void ones_0_48()   { assertThat(ones(subject,  0,       48), is(equalTo( 22))); }
+		@Test public void ones_0_56()   { assertThat(ones(subject,  0,       56), is(equalTo( 27))); }
+		@Test public void ones_0_60()   { assertThat(ones(subject,  0,       60), is(equalTo( 31))); }
+		@Test public void ones_0_62()   { assertThat(ones(subject,  0,       62), is(equalTo( 33))); }
+		@Test public void ones_0_64()   { assertThat(ones(subject,  0,       64), is(equalTo( 33))); }
+		@Test public void ones_0_72()   { assertThat(ones(subject,  0,       72), is(equalTo( 34))); }
+		@Test public void ones_0_80()   { assertThat(ones(subject,  0,       80), is(equalTo( 40))); }
+		@Test public void ones_0_88()   { assertThat(ones(subject,  0,       88), is(equalTo( 43))); }
+		@Test public void ones_0_92()   { assertThat(ones(subject,  0,       92), is(equalTo( 44))); }
+		@Test public void ones_0_96()   { assertThat(ones(subject,  0,       96), is(equalTo( 47))); }
+		@Test public void ones_0_112()  { assertThat(ones(subject,  0,      112), is(equalTo( 56))); }
+		@Test public void ones_0_120()  { assertThat(ones(subject,  0,      120), is(equalTo( 60))); }
+		@Test public void ones_0_124()  { assertThat(ones(subject,  0,      124), is(equalTo( 62))); }
+		@Test public void ones_0_128()  { assertThat(ones(subject,  0,      128), is(equalTo( 64))); }
+		@Test public void ones_0_144()  { assertThat(ones(subject,  0,      144), is(equalTo( 68))); }
+		@Test public void ones_0_160()  { assertThat(ones(subject,  0,      160), is(equalTo( 77))); }
+		@Test public void ones_0_176()  { assertThat(ones(subject,  0,      176), is(equalTo( 88))); }
+		@Test public void ones_0_184()  { assertThat(ones(subject,  0,      184), is(equalTo( 91))); }
+		@Test public void ones_0_192()  { assertThat(ones(subject,  0,      192), is(equalTo( 95))); }
+		@Test public void ones_0_224()  { assertThat(ones(subject,  0,      224), is(equalTo(113))); }
+		@Test public void ones_0_240()  { assertThat(ones(subject,  0,      240), is(equalTo(118))); }
+		@Test public void ones_0_248()  { assertThat(ones(subject,  0,      248), is(equalTo(123))); }
+		@Test public void ones_0_288()  { assertThat(ones(subject,  0,      288), is(equalTo(150))); }
+		@Test public void ones_2_4()    { assertThat(ones(subject,  2,  2 +   4), is(equalTo(  3))); }
+		@Test public void ones_2_6()    { assertThat(ones(subject,  2,  2 +   6), is(equalTo(  4))); }
+		@Test public void ones_2_7()    { assertThat(ones(subject,  2,  2 +   7), is(equalTo(  5))); }
+		@Test public void ones_2_12()   { assertThat(ones(subject,  2,  2 +  12), is(equalTo(  6))); }
+		@Test public void ones_2_14()   { assertThat(ones(subject,  2,  2 +  14), is(equalTo(  7))); }
+		@Test public void ones_2_15()   { assertThat(ones(subject,  2,  2 +  15), is(equalTo(  7))); }
+		@Test public void ones_2_20()   { assertThat(ones(subject,  2,  2 +  20), is(equalTo(  8))); }
+		@Test public void ones_2_22()   { assertThat(ones(subject,  2,  2 +  22), is(equalTo(  8))); }
+		@Test public void ones_2_23()   { assertThat(ones(subject,  2,  2 +  23), is(equalTo(  8))); }
+		@Test public void ones_2_28()   { assertThat(ones(subject,  2,  2 +  28), is(equalTo( 12))); }
+		@Test public void ones_2_30()   { assertThat(ones(subject,  2,  2 +  30), is(equalTo( 14))); }
+		@Test public void ones_2_31()   { assertThat(ones(subject,  2,  2 +  31), is(equalTo( 15))); }
+		@Test public void ones_2_36()   { assertThat(ones(subject,  2,  2 +  36), is(equalTo( 19))); }
+		@Test public void ones_4_8()    { assertThat(ones(subject,  4,  4 +   8), is(equalTo(  5))); }
+		@Test public void ones_4_12()   { assertThat(ones(subject,  4,  4 +  12), is(equalTo(  6))); }
+		@Test public void ones_4_14()   { assertThat(ones(subject,  4,  4 +  14), is(equalTo(  6))); }
+		@Test public void ones_4_24()   { assertThat(ones(subject,  4,  4 +  24), is(equalTo( 10))); }
+		@Test public void ones_4_28()   { assertThat(ones(subject,  4,  4 +  28), is(equalTo( 13))); }
+		@Test public void ones_4_30()   { assertThat(ones(subject,  4,  4 +  30), is(equalTo( 15))); }
+		@Test public void ones_4_40()   { assertThat(ones(subject,  4,  4 +  40), is(equalTo( 19))); }
+		@Test public void ones_4_44()   { assertThat(ones(subject,  4,  4 +  44), is(equalTo( 20))); }
+		@Test public void ones_4_46()   { assertThat(ones(subject,  4,  4 +  46), is(equalTo( 20))); }
+		@Test public void ones_4_48()   { assertThat(ones(subject,  4,  4 +  48), is(equalTo( 22))); }
+		@Test public void ones_4_56()   { assertThat(ones(subject,  4,  4 +  56), is(equalTo( 29))); }
+		@Test public void ones_4_60()   { assertThat(ones(subject,  4,  4 +  60), is(equalTo( 31))); }
+		@Test public void ones_4_62()   { assertThat(ones(subject,  4,  4 +  62), is(equalTo( 32))); }
+		@Test public void ones_4_72()   { assertThat(ones(subject,  4,  4 +  72), is(equalTo( 35))); }
+		@Test public void ones_8_16()   { assertThat(ones(subject,  8,  8 +  16), is(equalTo(  4))); }
+		@Test public void ones_8_24()   { assertThat(ones(subject,  8,  8 +  24), is(equalTo( 10))); }
+		@Test public void ones_8_28()   { assertThat(ones(subject,  8,  8 +  28), is(equalTo( 14))); }
+		@Test public void ones_8_48()   { assertThat(ones(subject,  8,  8 +  48), is(equalTo( 22))); }
+		@Test public void ones_8_56()   { assertThat(ones(subject,  8,  8 +  56), is(equalTo( 28))); }
+		@Test public void ones_8_60()   { assertThat(ones(subject,  8,  8 +  60), is(equalTo( 29))); }
+		@Test public void ones_8_80()   { assertThat(ones(subject,  8,  8 +  80), is(equalTo( 38))); }
+		@Test public void ones_8_88()   { assertThat(ones(subject,  8,  8 +  88), is(equalTo( 42))); }
+		@Test public void ones_8_92()   { assertThat(ones(subject,  8,  8 +  92), is(equalTo( 45))); }
+		@Test public void ones_8_112()  { assertThat(ones(subject,  8,  8 + 112), is(equalTo( 55))); }
+		@Test public void ones_8_120()  { assertThat(ones(subject,  8,  8 + 120), is(equalTo( 59))); }
+		@Test public void ones_8_124()  { assertThat(ones(subject,  8,  8 + 124), is(equalTo( 59))); }
+		@Test public void ones_8_144()  { assertThat(ones(subject,  8,  8 + 144), is(equalTo( 67))); }
+		@Test public void ones_16_32()  { assertThat(ones(subject, 16, 16 +  32), is(equalTo( 14))); }
+		@Test public void ones_16_48()  { assertThat(ones(subject, 16, 16 +  48), is(equalTo( 25))); }
+		@Test public void ones_16_56()  { assertThat(ones(subject, 16, 16 +  56), is(equalTo( 26))); }
+		@Test public void ones_16_96()  { assertThat(ones(subject, 16, 16 +  96), is(equalTo( 48))); }
+		@Test public void ones_16_112() { assertThat(ones(subject, 16, 16 + 112), is(equalTo( 56))); }
+		@Test public void ones_16_120() { assertThat(ones(subject, 16, 16 + 120), is(equalTo( 56))); }
+		@Test public void ones_16_160() { assertThat(ones(subject, 16, 16 + 160), is(equalTo( 80))); }
+		@Test public void ones_16_176() { assertThat(ones(subject, 16, 16 + 176), is(equalTo( 87))); }
+		@Test public void ones_16_184() { assertThat(ones(subject, 16, 16 + 184), is(equalTo( 94))); }
+		@Test public void ones_16_224() { assertThat(ones(subject, 16, 16 + 224), is(equalTo(110))); }
+		@Test public void ones_16_240() { assertThat(ones(subject, 16, 16 + 240), is(equalTo(119))); }
+		@Test public void ones_16_248() { assertThat(ones(subject, 16, 16 + 248), is(equalTo(123))); }
+		@Test public void ones_16_288() { assertThat(ones(subject, 16, 16 + 288), is(equalTo(155))); }
 	}
 }
