099543ab2b9f42914e7b66b5333b6c2e19b539b0
R. Lemos <rslemos@gmail.com>
2017-03-30 09:36:08 -0300

    introduce javadoc to ArithmeticCodec

--- src/main/java/br/eti/rslemos/bitsmagic/arithmetic/ArithmeticCodec.java
+++ src/main/java/br/eti/rslemos/bitsmagic/arithmetic/ArithmeticCodec.java
@@ -29,13 +29,94 @@ package br.eti.rslemos.bitsmagic.arithmetic;
 
 import java.io.IOException;
 
+/**
+ * An implementation of arithmetic coding.
+ * 
+ * <p>Arithmetic coding (or range coding) is a form of entropy encoding.
+ * </p>
+ * <p>The subinterfaces of this interface provide access to the {@link Encoder} 
+ * and {@link Decoder} aspects of the algorithm.
+ * </p>
+ * <p>Encoding usage as follows:
+ * </p>
+ * <pre>
+ *   {@link ArithmeticCodec.Encoder} encoder = ...;
+ *   encoder.write(symbol, cumulativeFrequencies);
+ *   ...
+ *   encoder.flush();
+ * </pre>
+ *
+ * <p>Decoding usage as follows:
+ * </p>
+ * <pre>
+ *   {@link ArithmeticCodec.Decoder} decoder = ...;
+ *   int symbol = read(cumulativeFrequencies);
+ *   ...
+ * </pre>
+ *
+ * @author Rodrigo Lemos
+ * @since 1.0.0
+ * 
+ * @see <a href="https://en.wikipedia.org/wiki/Arithmetic_coding">Arithmetic 
+ *   coding</a>
+ */
 public interface ArithmeticCodec {
+	/**
+	 * Encoding interface of an {@code ArithmeticCodec}.
+	 *
+	 * @since 1.0.0
+	 */
 	public interface Encoder {
+		/**
+		 * Writes a symbol of an alphabet with a given input probability 
+		 * distribution.
+		 *
+		 * @param v symbol to be written, between 0, inclusive, and 
+		 *        {@code cumulativeCount.length}, exclusive.
+		 * @param cumulativeCount discrete cumulative count function; 
+		 *        non-decreasing count of symbols {@code f(i)} up to symbol 
+		 *        {@code i}. Probability of each symbol {@code i} can be 
+		 *        computed as {@code (f(i) - f(i-1)) / (f(f.length - 1)) } 
+		 *        ({@code f(-1) = 0}). If {@code f(i) == f(i - 1)} then symbol 
+		 *        {@code i} is unexpected; in this case 
+		 *        {@code IllegalArgumentException} is thrown.
+		 * 
+		 * @since 1.0.0
+		 */
 		void write(int v, int... cumulativeCount) throws IOException;
+
+		/**
+		 * Flushes whatever internal state is stored in this Encoder. This 
+		 * Encoder should not be used anymore. Any other call to this Encoder 
+		 * invokes undefined behavior.
+		 * 
+		 * @since 1.0.0
+		 */
 		void flush() throws IOException;
 	}
 
+	/**
+	 * Decoding interface of an {@code ArithmeticCodec}.
+	 *
+	 * @since 1.0.0
+	 */
 	public interface Decoder {
+		/**
+		 * Reads a symbol of an alphabet with a given input probability 
+		 * distribution.
+		 *
+		 * @param cumulativeCount discrete cumulative count function; 
+		 *        non-decreasing count of symbols {@code f(i)} up to symbol 
+		 *        {@code i}. Probability of each symbol {@code i} can be 
+		 *        computed as {@code (f(i) - f(i-1)) / (f(f.length - 1)) } 
+		 *        ({@code f(-1) = 0}). If {@code f(i) == f(i - 1)} then symbol 
+		 *        {@code i} is unexpected; in this case 
+		 *        {@code IllegalArgumentException} is thrown.
+		 * @return symbol between 0, inclusive, and 
+		 *        {@code cumulativeCount.length}, exclusive.
+		 * 
+		 * @since 1.0.0
+		 */
 		int read(int... cumulativeCount) throws IOException;
 	}
 }
